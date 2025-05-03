fc5003712a693d43fb470c13af1189831b81b89e
Mohammad Hassani <Mohammad.Hassani.AA@gmail.com>
2025-05-03 14:36:29 +0330

    remove NavBarNotifications, NavSearchBar, NavbarShortcuts

--- src/layouts/components/DefaultLayoutWithHorizontalNav.vue
+++ src/layouts/components/DefaultLayoutWithHorizontalNav.vue
@@ -4,6 +4,11 @@ import { themeConfig } from '@themeConfig'
 
 // Components
 import Footer from '@/layouts/components/Footer.vue'
+
+// ***
+// import NavBarNotifications from '@/layouts/components/NavBarNotifications.vue'
+// import NavSearchBar from '@/layouts/components/NavSearchBar.vue'
+// import NavbarShortcuts from '@/layouts/components/NavbarShortcuts.vue'
 import NavbarThemeSwitcher from '@/layouts/components/NavbarThemeSwitcher.vue'
 import UserProfile from '@/layouts/components/UserProfile.vue'
 import NavBarI18n from '@core/components/I18n.vue'
@@ -27,6 +32,8 @@ import { VNodeRenderer } from '@layouts/components/VNodeRenderer'
       </RouterLink>
       <VSpacer />
 
+      <!-- *** -->
+      <!-- <NavSearchBar trigger-btn-class="ms-lg-n3" /> -->
 
       <NavBarI18n
         v-if="themeConfig.app.i18n.enable && themeConfig.app.i18n.langConfig?.length"
@@ -34,6 +41,11 @@ import { VNodeRenderer } from '@layouts/components/VNodeRenderer'
       />
 
       <NavbarThemeSwitcher />
+      <!-- *** -->
+      <!--
+        <NavbarShortcuts />
+        <NavBarNotifications class="me-2" /> 
+      -->
       <UserProfile />
     </template>
 
