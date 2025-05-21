16a2ade3d269122f1e45bc92d1c820a52363fdd4
Mohammad Hassani <Mohammad.Hassani.AA@gmail.com>
2025-05-21 10:52:18 +0330

    feat: add Shabnam font files and integrate into styles and webfontloader

--- src/plugins/webfontloader.js
+++ src/plugins/webfontloader.js
@@ -7,8 +7,13 @@ export async function loadFonts() {
   const webFontLoader = await import(/* webpackChunkName: "webfontloader" */ 'webfontloader')
 
   webFontLoader.load({
-    google: {
-      families: ['Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap'],
+    // google: {
+    //   families: ['Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap'],
+    // },
+
+    custom: {
+      families: ['Shabnam'],
+      urls: ['/src/assets/fonts/Shabnam/Shabnam.css'],
     },
   })
 }
