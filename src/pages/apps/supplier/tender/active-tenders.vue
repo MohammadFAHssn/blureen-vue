47e4b565e30c49380fc946772b98ef2ca29a8745
Mohammad Hassani <Mohammad.Hassani.AA@gmail.com>
2025-06-15 13:04:39 +0330

    feat: fetch active tenders from API instead of hardcoded values

--- src/pages/apps/supplier/tender/active-tenders.vue
+++ src/pages/apps/supplier/tender/active-tenders.vue
@@ -1,26 +1,37 @@
 <script setup>
 const router = useRouter()
 
-const tenders = [
-  {
-    id: 1,
-    title: "مناقصه برای لوازم اداری",
-    token: "09d5dc2e4b3705b5b8f718a88cc07885",
-  },
-  {
-    id: 2,
-    title: "مناقصه برای تجهیزات کامپیوتری",
-    token: "09d5dc2e4b3705b5b8f718a88cc07885",
-  },
-  {
-    id: 3,
-    title: "مناقصه برای خدمات نظافتی",
-    token: "09d5dc2e4b3705b5b8f718a88cc07885",
-  },
-]
+// --- Constants ---
+const API_BASE_PATH = import.meta.env.VITE_API_LEGACY_INTEGRATED_SYSTEM
+
+//
+const userData = useCookie("userData")
+
+const { data: apiData, error: apiError } = await useApi(
+  createUrl(
+    `${API_BASE_PATH}/tenderSMS/get-active-tenders-by-supplierId?supplier_id=${userData.value.id}`,
+  ),
+)
+
+if (apiError.value) {
+  hasError.value = true
+  errorMessage.value = apiError.value.message || "خطا در دریافت اطلاعات."
+}
+
+const tenders = apiData.value.data
 </script>
 
 <template>
+  <VSnackbar
+    v-model="hasError"
+    :timeout="2000"
+    location="center"
+    variant="flat"
+    color="error"
+  >
+    {{ errorMessage }}
+  </VSnackbar>
+
   <VRow>
     <VCol
       v-for="tender in tenders"
