3157c49b2285da1b6015595cea4242f4fbc193d6
Mohammad Hassani <Mohammad.Hassani.AA@gmail.com>
2025-08-09 10:50:01 +0330

    feat(payroll): add summary cards for total payments and deductions in payroll slip

--- src/pages/apps/payroll/payroll-slip.vue
+++ src/pages/apps/payroll/payroll-slip.vue
@@ -37,6 +37,58 @@ import Payments from '@/views/apps/payroll/Payments.vue'
         <VCol cols="4" class="pt-0">
           <OtherDetails />
         </VCol>
+
+        <VCol cols="4" class="pt-0 pl-0">
+          <VCard variant="tonal" color="success">
+            <VCardItem class="pa-4 pb-0">
+              <VCardTitle>جمع ناخالص پرداختی:</VCardTitle>
+            </VCardItem>
+
+            <VCardText class="pa-4 pt-0">
+              <div class="d-flex align-center justify-space-between gap-x-2">
+                <h4 class="text-h6 text-center">
+                  12,500,000
+                </h4>
+                <div class="text-sm text-success">
+                  +1.6%
+                </div>
+              </div>
+            </VCardText>
+          </VCard>
+        </VCol>
+
+        <VCol cols="4" class="pt-0 pl-0">
+          <VCard variant="tonal" color="error">
+            <VCardItem class="pa-4 pb-0">
+              <VCardTitle>جمع کسورات:</VCardTitle>
+            </VCardItem>
+
+            <VCardText class="pa-4 pt-0">
+              <h4 class="text-h6">
+                12,500,000-
+              </h4>
+            </VCardText>
+          </VCard>
+        </VCol>
+
+        <VCol cols="4" class="pt-0">
+          <VCard variant="tonal" color="primary">
+            <VCardItem class="pa-4 pb-0">
+              <VCardTitle>خالص پرداختی:</VCardTitle>
+            </VCardItem>
+
+            <VCardText class="pa-4 pt-0">
+              <div class="d-flex align-center justify-space-between gap-x-2">
+                <h4 class="text-h6 text-center">
+                  12,500,000
+                </h4>
+                <div class="text-sm text-success">
+                  +1.6%
+                </div>
+              </div>
+            </VCardText>
+          </VCard>
+        </VCol>
       </VRow>
     </section>
   </section>
