936bf6f1bf8ccda057019c91b7b2e08fb13850c6
Mohammad Hassani <Mohammad.Hassani.AA@gmail.com>
2025-09-14 12:43:09 +0330

    build

--- dist/assets/VSpacer-DltqhaA2.js
+++
@@ -1,1 +0,0 @@
-/* empty css              */import{c as r}from"./createSimpleFunctional-B7tEoKAB.js";const e=r("v-spacer","div","VSpacer");export{e as V};
--- dist/assets/tender-history-CW9GnZsP.js
+++
@@ -1,1 +0,0 @@
-import{M as e,c as t,o as c}from"./index-DcXQA0pX.js";const r={};function n(o,s){return c(),t("div",null,"test")}const _=e(r,[["render",n]]);export{_ as default};
