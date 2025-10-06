2e2f17bc4170e0df93cdf3dacfce06eabd510e8b
amir karimi <amir.hssn.karimi@gmail.com>
2025-10-06 14:28:40 +0330

    add birthday admin file management page

---
+++ src/pages/apps/payroll/birthday/birthday-admin-file.vue
@@ -0,0 +1,247 @@
+<script setup>
+import AreYouSureDialog from '@/components/dialogs/AreYouSureDialog.vue'
+import BirthdayFileCreateDialog from '@/views/apps/payroll/birthday/BirthdayFileCreateDialog.vue'
+import BirthdayFileDetailsDialog from '@/views/apps/payroll/birthday/BirthdayFileDetailsDialog.vue'
+
+definePage({
+  meta: {
+    layoutWrapperClasses: 'layout-content-height-fixed',
+  },
+})
+
+// states
+const uiState = reactive({
+  hasError: false,
+  errorMessage: '',
+  isBirthdayFileCreateDialogVisible: false,
+  isBirthdayFileDeleteDialogVisible: false,
+  isBirthdayFileDetailsDialogVisible: false,
+})
+
+const pendingState = reactive({
+  createBirthdayFile: false,
+  fetchingBirthdayFiles: false,
+  deleteBirthdayFile: false,
+  detailsBirthdayFile: false,
+})
+
+const birthdayFiles = ref([])
+
+const selectedNodes = ref([])
+const gridApi = ref(null)
+// ----- start ag-grid -----
+
+const { theme } = useAGGridTheme()
+
+function onGridReady(params) {
+  gridApi.value = params.api
+  gridApi.value.setGridOption('loading', true)
+}
+
+const columnDefs = ref([
+  { headerName: 'نام فایل', field: 'name' },
+  { headerName: 'ماه', field: 'month' },
+  { headerName: 'سال', field: 'year' },
+  {
+    headerName: 'وضعیت',
+    field: 'status',
+    cellRenderer: 'Active',
+    cellStyle: { 'display': 'flex', 'align-items': 'center' },
+    filterParams: {
+      valueFormatter: params => (params.value === 1 ? 'فعال' : 'غیرفعال'),
+    },
+  },
+  { headerName: 'بارگذاری توسط', field: 'uploadedBy' },
+  { headerName: 'آخرین ویرایش توسط', field: 'editedBy' },
+  {
+    headerName: 'تاریخ بارگذاری',
+    field: 'createdAt',
+    valueFormatter: (params) =>
+      moment(params.value, 'jYYYY-jMM-jDD HH:mm:ss').format(
+        'jYYYY/jMM/jD HH:mm:ss',
+      ),
+  },
+  {
+    headerName: 'تاریخ آخرین ویرایش',
+    field: 'updatedAt',
+    valueFormatter: (params) =>
+      moment(params.value, 'jYYYY-jMM-jDD HH:mm:ss').format(
+        'jYYYY/jMM/jD HH:mm:ss',
+      ),
+  },
+  {
+    headerName: 'عملیات',
+    field: 'actions',
+    cellRendererSelector: (params) => {
+      return {
+        component: 'Actions',
+        params: {
+          onDeleteClick: (selectedNode) => {
+            selectedNodes.value = [selectedNode]
+            uiState.isBirthdayFileDeleteDialogVisible = true
+          },
+          onDetailsClick: (selectedNode) => {
+            selectedNodes.value = [selectedNode]
+            uiState.isBirthdayFileDetailsDialogVisible = true
+          },
+        },
+      }
+    },
+  },
+])
+
+const rowData = computed(() =>
+  birthdayFiles.value?.map((file) => {
+    return {
+      id: file.id,
+      name: file.name,
+      month: file.month,
+      year: file.year,
+      status: file.status,
+      uploadedBy: file.uploadedBy?.fullName || '--',
+      editedBy: file.editedBy?.fullName || '--',
+      createdAt: moment(file.createdAt).format('jYYYY-jMM-jDD HH:mm:ss'),
+      updatedAt: moment(file.updatedAt).format('jYYYY-jMM-jDD HH:mm:ss'),
+      actions: {
+        deletable: true,
+        detailsable: true,
+      },
+    }
+  }),
+)
+
+// ----- end ag-grid -----
+
+// ----- -----
+
+async function fetchbirthdayFiles() {
+  pendingState.fetchingBirthdayFiles = true
+  gridApi.value?.setGridOption('loading', true)
+  try {
+    const res = await $api('/birthday/file/', { method: 'GET' })
+
+    birthdayFiles.value = res?.data?.birthdayFiles || []
+  } catch (e) {
+    console.error('Error fetching birthdayFiles:', e)
+    uiState.hasError = true
+    uiState.errorMessage = e.message || 'خطا در دریافت فایل‌های هدیه'
+  } finally {
+    pendingState.fetchingBirthdayFiles = false
+    gridApi.value?.setGridOption('loading', false)
+  }
+}
+
+fetchbirthdayFiles()
+
+async function onCreateBirthdayFile(payload) {
+  const formData = new FormData()
+
+  formData.append('month', Number(payload.birthdayFileDate.split('/')[1]))
+  formData.append('year', Number(payload.birthdayFileDate.split('/')[0]))
+
+  formData.append('file', payload.birthdayFile)
+  formData.append('file_name', payload.birthdayFile.name)
+
+  pendingState.createBirthdayFile = true
+  try {
+    await $api('/birthday/file/', {
+      method: 'POST',
+      body: formData,
+      onResponseError({ response }) {
+        uiState.hasError = true
+        if (response._data?.errors) {
+          const errors = Object.values(response._data.errors)
+            .flat()
+            .join(' | ')
+          uiState.errorMessage = errors
+        } else {
+          uiState.errorMessage =
+            response._data?.message || 'خطا در آپلود فایل هدیه'
+        }
+      },
+    })
+
+    uiState.isBirthdayFileCreateDialogVisible = false
+  } catch (err) {
+    console.error(err)
+  } finally {
+    pendingState.createBirthdayFile = false
+    fetchbirthdayFiles()
+  }
+}
+
+async function onDelete() {
+  const id = selectedNodes.value[0].data.id
+  pendingState.deleteBirthdayFile = true
+  try {
+    await $api(`/birthday/file/${id}`, {
+      method: 'DELETE',
+      onResponseError({ response }) {
+        pendingState.deleteBirthdayFile = false
+        uiState.hasError = true
+        uiState.errorMessage = response._data.message || 'خطا در حذف فایل هدیه'
+      },
+    })
+
+    pendingState.deleteBirthdayFile = false
+    uiState.isBirthdayFileDeleteDialogVisible = false
+    birthdayFiles.value = birthdayFiles.value.filter(
+      (file) => file.id !== selectedNodes.value[0].data.id,
+    )
+  } catch (err) {
+    console.error(err)
+  }
+}
+</script>
+
+<template>
+  <VLayout class="app-layout">
+    <VSnackbar v-model="uiState.hasError" :timeout="2000" location="center" variant="flat" color="error">
+      {{ uiState.errorMessage }}
+    </VSnackbar>
+
+    <section style="block-size: 100%">
+      <AgGridVue style="block-size: 100%; inline-size: 100%" :column-defs="columnDefs" :row-data="rowData" enable-rtl
+        row-numbers pagination :theme="theme" @grid-ready="onGridReady" />
+    </section>
+
+    <BirthdayFileCreateDialog v-if="uiState.isBirthdayFileCreateDialogVisible"
+      v-model:is-dialog-visible="uiState.isBirthdayFileCreateDialogVisible" :loading="pendingState.createBirthdayFile"
+      @submit="onCreateBirthdayFile" />
+
+    <BirthdayFileDetailsDialog v-if="uiState.isBirthdayFileDetailsDialogVisible"
+      v-model:is-dialog-visible="uiState.isBirthdayFileDetailsDialogVisible" :loading="pendingState.detailsBirthdayFile"
+      @submit="onCreateBirthdayFile" />
+
+    <AreYouSureDialog v-if="uiState.isBirthdayFileDeleteDialogVisible"
+      v-model:is-dialog-visible="uiState.isBirthdayFileDeleteDialogVisible"
+      title="آیا از حذف این فایل هدیه اطمینان دارید؟" :loading="pendingState.deleteBirthdayFile" @confirm="onDelete" />
+
+    <VApp>
+      <VFab app icon="tabler-plus" size="x-large" @click="uiState.isBirthdayFileCreateDialogVisible = true" />
+    </VApp>
+  </VLayout>
+</template>
+
+<style lang="scss" scoped>
+@use '@styles/variables/vuetify';
+@use '@core/scss/base/mixins';
+
+.app-layout {
+  @include mixins.elevation(vuetify.$card-elevation);
+
+  display: grid;
+  grid-template-columns: auto;
+  grid-template-rows: 1fr auto;
+}
+
+.v-application {
+  max-block-size: 100%;
+  min-block-size: 100%;
+}
+
+:deep(.v-application__wrap) {
+  max-block-size: 100% !important;
+  min-block-size: 100% !important;
+}
+</style>
--- typed-router.d.ts
+++ typed-router.d.ts
@@ -19,6 +19,9 @@ declare module 'vue-router/auto-routes' {
    */
   export interface RouteNamedMap {
     '$error': RouteRecordInfo<'$error', '/:error(.*)', { error: ParamValue<true> }, { error: ParamValue<false> }>,
+    'apps-payroll-birthday-birthday-admin-file': RouteRecordInfo<'apps-payroll-birthday-birthday-admin-file', '/apps/payroll/birthday/birthday-admin-file', Record<never, never>, Record<never, never>>,
+    'apps-payroll-birthday-birthday-admin-gift': RouteRecordInfo<'apps-payroll-birthday-birthday-admin-gift', '/apps/payroll/birthday/birthday-admin-gift', Record<never, never>, Record<never, never>>,
+    'apps-payroll-birthday-birthday-user': RouteRecordInfo<'apps-payroll-birthday-birthday-user', '/apps/payroll/birthday/birthday-user', Record<never, never>, Record<never, never>>,
     'apps-payroll-payroll-batches-management': RouteRecordInfo<'apps-payroll-payroll-batches-management', '/apps/payroll/payroll-batches-management', Record<never, never>, Record<never, never>>,
     'apps-payroll-payroll-slip': RouteRecordInfo<'apps-payroll-payroll-slip', '/apps/payroll/payroll-slip', Record<never, never>, Record<never, never>>,
     'apps-permissions': RouteRecordInfo<'apps-permissions', '/apps/permissions', Record<never, never>, Record<never, never>>,
@@ -50,6 +53,18 @@ declare module 'vue-router/auto-routes' {
   export interface _RouteFileInfoMap {
     'src/pages/[...error].vue': {
       routes: '$error'
+      views: never
+    }
+    'src/pages/apps/payroll/birthday/birthday-admin-file.vue': {
+      routes: 'apps-payroll-birthday-birthday-admin-file'
+      views: never
+    }
+    'src/pages/apps/payroll/birthday/birthday-admin-gift.vue': {
+      routes: 'apps-payroll-birthday-birthday-admin-gift'
+      views: never
+    }
+    'src/pages/apps/payroll/birthday/birthday-user.vue': {
+      routes: 'apps-payroll-birthday-birthday-user'
       views: never
     }
     'src/pages/apps/payroll/payroll-batches-management.vue': {
