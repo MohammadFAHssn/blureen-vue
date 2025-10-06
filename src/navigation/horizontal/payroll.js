0edb7830cc0f68678bb23659836f70be5e00bf56
amir karimi <amir.hssn.karimi@gmail.com>
2025-10-06 08:22:41 +0330

    update navigation menu for payroll: add menu for birthday gift

--- src/navigation/horizontal/payroll.js
+++ src/navigation/horizontal/payroll.js
@@ -19,6 +19,35 @@ export default [
         subject: 'Payroll-Batches',
         to: 'apps-payroll-payroll-batches-management',
       },
+      {
+        title: 'Payroll Birthday',
+        icon: { icon: 'tabler-gift' },
+        action: 'use',
+        subject: 'app',
+        to: 'apps-payroll-birthday-birthday-user',
+      },
+      {
+        title: 'Payroll Birthday Management',
+        icon: { icon: 'tabler-adjustments' },
+        action: 'use',
+        subject: 'app',
+        children: [
+          {
+            title: 'Payroll Birthday Management file',
+            icon: { icon: 'tabler-file' },
+            action: 'use',
+            subject: 'app',
+            to: 'apps-payroll-birthday-birthday-admin-file',
+          },
+          {
+            title: 'Payroll Birthday Management gift',
+            icon: { icon: 'tabler-gift' },
+            action: 'use',
+            subject: 'app',
+            to: 'apps-payroll-birthday-birthday-admin-gift',
+          },
+        ]
+      },
     ],
   },
 
