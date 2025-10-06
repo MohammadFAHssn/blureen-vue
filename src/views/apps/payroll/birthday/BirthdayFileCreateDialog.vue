492a07e0ceb5eb0e91b6fb3697eb4a508a605774
amir karimi <amir.hssn.karimi@gmail.com>
2025-10-06 14:29:40 +0330

    add Dialog for Uploading new birthday excel file

---
+++ src/views/apps/payroll/birthday/BirthdayFileCreateDialog.vue
@@ -0,0 +1,116 @@
+<script setup>
+const props = defineProps({
+  isDialogVisible: {
+    type: Boolean,
+    required: true,
+  },
+
+  loading: {
+    type: Boolean,
+    required: true,
+  },
+})
+
+const emit = defineEmits(['submit', 'update:isDialogVisible'])
+
+const constants = inject('constants')
+
+// states
+const birthdayFileDate = ref(null)
+const refVForm = ref()
+const birthdayFile = ref(null)
+
+//
+
+const fileInputRules = [
+  (fileList) => {
+    // if no file is selected yet
+    if (!fileList.length) {
+      return true
+    }
+
+    const extension = fileList[0].name.split('.').pop().toLowerCase()
+    if (extension !== 'xlsx' && extension !== 'xls') {
+      return 'لطفا یک فایل اکسل معتبر انتخاب کنید!'
+    }
+
+    if (fileList[0].size > constants.MAX_FILE_SIZE) {
+      return 'حجم فایل باید کمتر از 5 مگابایت باشد!'
+    }
+
+    return true
+  },
+]
+
+const datePickerRules = [
+  () => !!birthdayFileDate.value || 'لطفا دوره هدیه را انتخاب کنید!',
+]
+
+// methods
+
+function onFormSubmit() {
+  refVForm.value?.validate().then(({ valid: isValid }) => {
+    if (isValid) {
+      emit('submit', {
+        birthdayFileDate: birthdayFileDate.value,
+        birthdayFile: birthdayFile.value,
+      })
+    }
+  })
+}
+
+function onFormReset() {
+  // userData.value = structuredClone(toRaw(props.userData))
+  emit('update:isDialogVisible', false)
+}
+
+function dialogModelValueUpdate(val) {
+  emit('update:isDialogVisible', val)
+}
+</script>
+
+<template>
+  <VDialog :width="$vuetify.display.smAndDown ? 'auto' : 900" :model-value="props.isDialogVisible"
+    @update:model-value="dialogModelValueUpdate">
+    <!-- Dialog close btn -->
+    <DialogCloseBtn @click="dialogModelValueUpdate(false)" />
+
+    <VCard>
+      <VCardText>
+        <!-- 👉 Title -->
+        <h4 class="text-h5 text-center mb-2">
+          افزودن فایل هدیه جدید
+        </h4>
+
+        <!-- 👉 Form -->
+        <VForm ref="refVForm" class="mt-6" validate-on="submit lazy" @submit.prevent="onFormSubmit">
+          <VRow>
+            <!-- Time Period -->
+            <VCol cols="12" md="6">
+              <PersianDatetimePicker v-model="birthdayFileDate" :disabled="loading" type="year-month" simple
+                label="دوره هدیه" />
+            </VCol>
+
+            <!-- Excel File -->
+            <VCol cols="12" md="6">
+              <VFileInput v-model="birthdayFile" :disabled="loading" label="فایل اکسل" accept=".xlsx, .xls"
+                :rules="[requiredValidator, ...fileInputRules]" />
+            </VCol>
+
+            <!-- Submit and Cancel -->
+            <VCol cols="12" class="d-flex flex-wrap justify-center gap-4">
+              <VBtn type="submit" :disabled="loading" :loading="loading" @click="refVForm?.validate()">
+                ذخیره
+              </VBtn>
+
+              <VBtn color="secondary" variant="tonal" @click="onFormReset">
+                انصراف
+              </VBtn>
+            </VCol>
+          </VRow>
+        </VForm>
+
+      </VCardText>
+    </VCard>
+  </VDialog>
+</template>
