9a6bad051f678194e23a896ff18ab172ea539a76
amir karimi <amir.hssn.karimi@gmail.com>
2025-10-09 14:54:34 +0330

    add Amount.vue

---
+++ src/plugins/3.ag-grid/components/Amount.vue
@@ -0,0 +1,9 @@
+<template>
+  <VChip v-if="params.value > 0" color="success" :label="false">
+    موجود
+  </VChip>
+
+  <VChip v-if="params.value <= 0" color="error" :label="false">
+    ناموجود
+  </VChip>
+</template>
