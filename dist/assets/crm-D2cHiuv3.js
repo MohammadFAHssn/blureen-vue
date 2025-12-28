4bb123822adc9e03e8c4bb052814a5baf31e9800
Mohammad Hassani <Mohammad.Hassani.AA@gmail.com>
2025-12-28 12:12:41 +0330

    build

---
+++ dist/assets/crm-D2cHiuv3.js
@@ -0,0 +1,1 @@
+import{V as o}from"./VRow-J48OAN-Z.js";import{f as r,o as t}from"./index-DfzFnyP4.js";/* empty css              */const _={__name:"crm",setup(e){return(a,c)=>(t(),r(o))}};export{_ as default};
---
+++ dist/assets/useAbility-wzr-eFIW.js
@@ -0,0 +1,1 @@
+import{cM as i}from"./index-DfzFnyP4.js";const t=()=>i();export{t as u};
