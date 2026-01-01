71fac46990b8752a0f6b732914cfb53bba2ef51d
Mohammad Hassani <Mohammad.Hassani.AA@gmail.com>
2026-01-01 13:14:35 +0330

    build

--- dist/assets/crm-Dbv_jsiP.js
+++
@@ -1,1 +0,0 @@
-import{V as o}from"./VRow-q8c3e74I.js";import{f as r,o as t}from"./index-Dxc-wXXq.js";/* empty css              */const _={__name:"crm",setup(e){return(a,c)=>(t(),r(o))}};export{_ as default};
--- dist/assets/index-DdB8zT18.js
+++
@@ -1,1 +0,0 @@
-import{g as a,bV as r}from"./index-Dxc-wXXq.js";var s=r();const o=a(s);export{o as j};
