4a7faecdad082bcb075d5642806c8b27aed26b27
mahdi <mahdi.zare.mehrjardi75@gmail.com>
2026-01-10 13:34:49 +0330

    navigations

--- src/navigation/horizontal/index.js
+++ src/navigation/horizontal/index.js
@@ -1,5 +1,6 @@
 import contractor from './contractor'
 import evaluationAndSurvey from './evaluation-and-survey'
+import food from './food.js'
 import home from './home'
 import hse from './hse'
 import humanResources from './humanResources.js'
@@ -8,6 +9,7 @@ import suppliers from './suppliers'
 import userManagement from './userManagement'
 
 export default [
+  ...food,
   ...home,
   ...contractor,
   ...payroll,
