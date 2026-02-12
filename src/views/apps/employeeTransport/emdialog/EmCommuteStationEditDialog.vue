0396394ed1ae9ef249d2e2a9be31590d076b24a2
amir karimi <amir.hssn.karimi@gmail.com>
2026-02-12 11:10:26 +0330

    add EmCommuteStationEditDialog

---
+++ src/views/apps/employeeTransport/emdialog/EmCommuteStationEditDialog.vue
@@ -0,0 +1,153 @@
+<script setup>
+const props = defineProps({
+  isDialogVisible: {
+    type: Boolean,
+    required: true,
+  },
+
+  loading: {
+    type: Boolean,
+    required: true,
+  },
+
+  cities: {
+    type: Array,
+    required: true,
+  },
+
+  station: {
+    type: Object,
+    required: true,
+  },
+})
+
+const emit = defineEmits(['submit', 'update:isDialogVisible'])
+
+// states
+const refVForm = ref()
+
+const cities = ref(props.cities)
+const station = ref(props.station)
+
+const city = ref(station.value.city)
+const address = ref(station.value.address)
+const code = ref(station.value.code)
+
+// methods
+function onFormSubmit() {
+  refVForm.value?.validate().then(({ valid: isValid }) => {
+    if (isValid) {
+      emit('submit', {
+        cityName: city.value,
+        address: address.value,
+        code: code.value,
+      })
+    }
+  })
+}
+
+function onFormReset() {
+  emit('update:isDialogVisible', false)
+}
+
+function dialogModelValueUpdate(val) {
+  emit('update:isDialogVisible', val)
+}
+
+function onCityComboboxUpdate(selectedItem) {
+  if (typeof selectedItem !== 'string') return
+
+  if (!selectedItem.trim()) {
+    city.value = null
+    return
+  }
+
+  const existing = cities.value.find(
+    city => toComparisonKey(city.name) === toComparisonKey(selectedItem),
+  )
+  if (existing) {
+    city.value = existing
+  }
+}
+function toComparisonKey(str) {
+  return (str || '').toString().replace(/[\s\u200C]+/g, '')
+}
+</script>
+
+<template>
+  <VDialog
+    :width="$vuetify.display.smAndDown ? 'auto' : 900"
+    :model-value="props.isDialogVisible"
+    @update:model-value="dialogModelValueUpdate"
+  >
+    <DialogCloseBtn @click="dialogModelValueUpdate(false)" />
+
+    <VCard>
+      <VCardText>
+        <h4 class="text-h5 text-center mb-2">
+          ویرایش ایستگاه
+        </h4>
+
+        <!-- 👉 Form -->
+        <VForm
+          ref="refVForm"
+          class="mt-6"
+          validate-on="submit lazy"
+          @submit.prevent="onFormSubmit"
+        >
+          <VRow>
+            <!-- city -->
+            <VCol cols="12" md="6">
+              <VCombobox
+                v-model="city"
+                label="شهر"
+                :items="cities"
+                :disabled="loading"
+                item-title="name"
+                item-value="id"
+                :rules="[requiredValidator]"
+                @update:model-value="onCityComboboxUpdate"
+              />
+            </VCol>
+
+            <!-- code -->
+            <VCol cols="12" md="6">
+              <VTextField
+                v-model="code"
+                :rules="[requiredValidator]"
+                :disabled="loading"
+                label="کد"
+              />
+            </VCol>
+
+            <!-- address -->
+            <VCol cols="12" md="12">
+              <VTextarea
+                v-model="address"
+                :rules="[requiredValidator]"
+                :disabled="loading"
+                label="آدرس"
+              />
+            </VCol>
+
+            <!-- Submit and Cancel -->
+            <VCol cols="12" class="d-flex flex-wrap justify-center gap-4">
+              <VBtn
+                type="submit"
+                :disabled="loading"
+                :loading="loading"
+                @click="refVForm?.validate()"
+              >
+                ذخیره
+              </VBtn>
+
+              <VBtn color="secondary" variant="tonal" @click="onFormReset">
+                انصراف
+              </VBtn>
+            </VCol>
+          </VRow>
+        </VForm>
+      </VCardText>
+    </VCard>
+  </VDialog>
+</template>
