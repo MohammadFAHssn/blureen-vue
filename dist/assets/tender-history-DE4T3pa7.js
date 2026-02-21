7ed51d788242a476903d658dfd24a41b1a7d7515
Mohammad Hassani <Mohammad.Hassani.AA@gmail.com>
2026-02-21 13:32:03 +0330

    build

---
+++ dist/assets/tender-history-DE4T3pa7.js
@@ -0,0 +1,1 @@
+import{M as e,c as t,o as c}from"./index-C-y62ulH.js";const r={};function n(o,s){return c(),t("div",null,"test")}const _=e(r,[["render",n]]);export{_ as default};
---
+++ dist/assets/useAbility-C8zpw-RU.js
@@ -0,0 +1,1 @@
+import{cE as i}from"./index-C-y62ulH.js";const t=()=>i();export{t as u};
