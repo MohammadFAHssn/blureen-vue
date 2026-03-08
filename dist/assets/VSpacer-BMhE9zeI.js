a8e7d2f9428544a5e04059fa88b7fb367cf4d28c
Mohammad Hassani <Mohammad.Hassani.AA@gmail.com>
2026-03-08 13:04:06 +0330

    build

---
+++ dist/assets/VSpacer-BMhE9zeI.js
@@ -0,0 +1,1 @@
+/* empty css              */import{c as r}from"./createSimpleFunctional-B4KT0eIK.js";const e=r("v-spacer","div","VSpacer");export{e as V};
---
+++ dist/assets/crm-BaBjWzKI.js
@@ -0,0 +1,1 @@
+import{V as o}from"./VRow-eZ4MEyvB.js";import{f as r,o as t}from"./index-ChZ0irFC.js";/* empty css              */const _={__name:"crm",setup(e){return(a,c)=>(t(),r(o))}};export{_ as default};
