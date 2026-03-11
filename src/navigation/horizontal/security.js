de7b901a43cacbf9aae0371701daff952148c70b
mahdi <mahdi.zare.mehrjardi75@gmail.com>
2026-03-12 00:38:11 +0330

    security cartable

---
+++ src/navigation/horizontal/security.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    title: 'Security',
+    icon: { icon: 'tabler-users' },
+    action: 'use',
+    subject: 'app',
+    children: [
+      {
+        title: 'Personnel Attendance',
+        to: 'apps-human-resources-requests',
+        icon: { icon: 'tabler-arrows-left-right' },
+        action: 'use',
+        subject: 'app',
+      },
+    ],
+  },
+]
---
+++ src/navigation/vertical/security.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    heading: 'Security',
+    action: 'use',
+    subject: 'app',
+  },
+  {
+    title: 'Personnel Attendance',
+    to: 'apps-human-resources-security-cartable',
+    icon: { icon: 'tabler-arrows-left-right' },
+    action: 'use',
+    subject: 'app',
+  },
+]
